f745ae4633fd6c3c1bb4af117fc365603e658866
Abdikani98 <abdikanishaarib>
2025-02-01 19:25:59 -0800

    Collaborate

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Footer from './components/footer'
+import Header from './components/header'
+import Nav from './components/nav'
+
+function App() {
+  return <div>
+    <Header />
+    <Nav />
+   
+    <Footer />
+    
+  </div>
+
+ 
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,29 @@
+const Header =() =>{
+    return <div>
+        <div >
+        <div className="flex gap-10 justify-between px-5 py-3" >
+            <div className="flex gap-3">
+                <div >
+                    <button className="bg-red-500 w-10 text-2xl rounded-lg text-white " ><i class="fa-solid fa-car"></i></button>
+                </div>
+                <h1 className="font-semibold">Le wango</h1>
+
+            </div>
+            <ul className="flex gap-10">
+                <li>Courses <i class="fa-solid fa-chevron-down"></i></li>
+                <li>Locations <i class="fa-solid fa-chevron-down"></i></li>
+                <li>Why Lo wagno</li>
+                <li> Events</li>
+                <li> Enters <i class="fa-solid fa-chevron-down"></i></li>
+            </ul>
+
+            <div>
+                <button className="bg-indigo-700 px-3 text-white font-semibold rounded-lg py-1" >Apply</button>
+            </div>
+
+        </div>
+
+     </div>
+    </div>
+}
+export default Header
